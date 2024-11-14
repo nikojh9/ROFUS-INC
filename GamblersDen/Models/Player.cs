e1d103c23821294b5c88211c1ca42ee38c929204
nikojh9 <nikolaijuhl@gmail.com>
2024-11-14 12:39:52 +0100

    +Tilføjet første udkast af player klassen

---
+++ GamblersDen/Models/Player.cs
@@ -0,0 +1,22 @@
+﻿namespace GamblersDen.Models
+{
+    public class Player
+    {
+
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public int Dollars { get; set; } = 500;
+        public int InvestmentReturns { get; set; } = 0;
+
+        //public List<Investments> boughtInvestments { get; set; }
+
+        //public List<Games> boughtGames { get; set; }
+
+
+        public Player(string username, string password) {
+            UserName = username;
+            Password = password;
+        }
+
+    }
+}
