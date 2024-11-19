7a25ac4049eee8ad8bb76a4508a6436df3a0a364
nikojh9 <nikolaijuhl@gmail.com>
2024-11-19 15:50:45 +0100

    +coinflip template view

---
+++ GamblersDen/Controllers/GamesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GamblersDen.Controllers
+{
+    public class GamesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+
+        [HttpGet]
+        [Route("/games/Coinflip")]
+        public IActionResult Coinflip()
+        {
+            return View();
+        }
+
+
+    }
+
+
+
+}
